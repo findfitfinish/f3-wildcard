d65dcf044a8156dbd333bc4e179e0f5a2ceafbcd
findfitfinish <55020499+findfitfinish@users.noreply.github.com>
2019-09-17 10:35:07 +0530

    updated code

--- flask_app.py
+++ flask_app.py
@@ -15,8 +15,5 @@ def submitAnswer():
 @app.route('/')
 def home():
     return render_template("qpage.html")
-@app.route("/259")
-def won():
-    return render_template("youwon.html")
 if __name__ == "__main__":
     app.run(debug=True)
